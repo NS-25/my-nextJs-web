12a32243756ddcc420c33e7268d12e752a6d908f
George-25 <georgemallory26@gmail.com>
2025-03-14 00:13:26 -0400

    created usestate and onchange event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
   return (
     <div>
       <div>
         <h1>Enter your Name</h1>
       </div>
       <form action="">
-        <input type="text" placeholder="Type your name..." />
+        <input
+          type="text"
+          placeholder="Type your name..."
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
         <button type="submit">Predict Data</button>
       </form>
     </div>
